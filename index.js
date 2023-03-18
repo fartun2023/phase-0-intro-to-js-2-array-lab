b258a6c2087ab6b7b18d897aa264e3c3ef7d72c0
fartun <fartunwario34@gmail.com>
2023-03-18 17:15:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+  }
+  function destructivelyAppendCat(params) {
+    cats.push('Ralph');
+  }
+  function destructivelyRemoveLastCat(params) {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat(params) {
+    cats.shift();
+  }
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  function prependCat(name) {
+    return [name, ...cats ];
+  }
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
+
+
